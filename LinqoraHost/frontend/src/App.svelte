fc9df5ce133d7912749940ca4d4733d1a6ebf91d
pasichDev <apasichnik9@gmail.com>
2025-04-18 22:12:59 +0300

    add @svelte-i18n

--- LinqoraHost/frontend/src/App.svelte
+++ LinqoraHost/frontend/src/App.svelte
@@ -11,21 +11,24 @@ import Settings from './routes/Settings.svelte';
 import Router, {
     location
 } from 'svelte-spa-router';
-
+import { _ } from 'svelte-i18n';
 
 const routes = {
     '/': Home,
     '/settings': Settings
 };
+
 </script>
 
 <SvelteUIProvider withGlobalStyles themeObserver={'dark'}>
 
     <AppShell padding={0}>
+    
         <Header slot="header" zIndex={10} height={24} >    <TitleBar /></Header>
         <slot>
 
             <Router {routes} />
 
         </slot>
-    </AppShell></SvelteUIProvider>
+        </AppShell>  </SvelteUIProvider>
+  
--- LinqoraHost/frontend/src/components/TitleBar.svelte
+++ LinqoraHost/frontend/src/components/TitleBar.svelte
@@ -1,17 +1,21 @@
 <script lang="ts">
 import {
+    _
+} from 'svelte-i18n';
+import {
     Group,
     Button,
     Text,
-    Paper
+    Paper,
+    Tooltip
 } from '@svelteuidev/core';
 import * as runtime from '../../wailsjs/runtime/runtime';
 import {
     SettingsAdjust,
     Close,
     Minimize
-} from "carbon-icons-svelte";
 
+} from "carbon-icons-svelte";
 const minimize = () => {
     runtime.WindowMinimise();
 };
@@ -25,18 +29,26 @@ const close = () => {
     <Paper shadow="md" padding={2} >
         <Group position="apart" >
             <Text size="sm" weight="bold">LinqoraHost</Text>
-
             <Group spacing="xs" class="no-drag">
-                <Button variant="subtle" color="gray" size="xs" href="#/settings">
-                    <SettingsAdjust />
-                </Button>
-                <Button variant="subtle" on:click={minimize} size="xs">
-                    <Minimize />
-                </Button>
-                <Button variant="subtle" color="red" on:click={close} size="xs">
-                    <Close />
-                </Button>
-            </Group>
-        </Group>
-    </Paper>
-</header>
+                <Tooltip label={$_("settings")} openDelay={500}>
+                    <Button variant="subtle" color="gray" size="xs" href="#/settings">
+                        <SettingsAdjust />
+                    </Button>
+                </Tooltip>
+                
+                <Tooltip label={$_("minimize")} openDelay={500}>
+                    <Button variant="subtle" on:click={minimize} size="xs">
+                        <Minimize />
+                    </Button>
+                </Tooltip>
+                
+                <Tooltip label={$_("closeApp")} openDelay={500}>
+                    <Button variant="subtle" color="red" on:click={close} size="xs">
+                        <Close />
+                    </Button>
+                </Tooltip>
+                
+                        </Group>
+                        </Group>
+                        </Paper>
+                        </header>
