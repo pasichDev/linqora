6d905f19732afdc2147eda95f983d6d1a0f7bc67
pasichDev <apasichnik9@gmail.com>
2025-05-21 10:49:58 +0300

    fix debug mode

--- linqoraremote/lib/core/constants/constants.dart
+++ linqoraremote/lib/core/constants/constants.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/foundation.dart';
+
 /// Allows the use of self-signed certificates for SSL connections.
 /// Set to `true` to permit self-signed certificates, which can be useful
 /// for development or testing environments.
 /// Note: This should be set to `false` in production for security reasons.
 const bool allowSelfSigned = true;
 
-const bool showErrorSu = false;
+const bool showErrorSu = kDebugMode;
